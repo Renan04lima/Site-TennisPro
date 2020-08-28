772eca72e369283953c7adb46eb359c496d77069
Renan Oliveira <renan04lima@outlook.com>
2020-08-28 18:45:29 -0300

    build test

---
+++ site/web/build/precache-manifest.d2caa3c3235b93ef59ec27390b03cb2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6de243f2c0d85ae671e413a702bdf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d95961ea45adb1afa8",
+    "url": "/static/css/main.b7a5e840.chunk.css"
+  },
+  {
+    "revision": "3de7434372b9bca042e1",
+    "url": "/static/js/2.4e939d8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e939d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d95961ea45adb1afa8",
+    "url": "/static/js/main.b4bdb134.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "4128788013032cb2940c52b89df93f82",
+    "url": "/static/media/appleStore.41287880.png"
+  },
+  {
+    "revision": "3bce00ef84e6abf5a9f4d2370df9aef7",
+    "url": "/static/media/teste.3bce00ef.png"
+  }
+]);
